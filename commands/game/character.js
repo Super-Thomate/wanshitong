72f880a82d71e488f512c15b7791fb33fcb4572a
Thomas Plaindoux <t.plaindoux@cartoon-fantasy.net>
2023-05-28 01:27:27 +0200

    add character show

--- commands/game/character.js
+++ commands/game/character.js
@@ -15,12 +15,32 @@ const data = new SlashCommandBuilder()
     )
     ;
 
-
 module.exports = {
   data: data,
   async execute(interaction) {
     await interaction.deferReply();
     const id = interaction.options.getInteger('id') ;
-    await interaction.editReply('Soon TM');
+    const personnage = await Personnage.findOne({
+      where: {id: id},
+      include: Item
+    }) ;
+    if (! personnage) {
+      return await interaction.editReply(`No character found with id ${id}.`);
+    }
+    const characterEmbed = {
+      color: 0xDDA624,
+      title: `${personnage.name} [${interaction.client.getRarityCharacter(personnage.rarity)}]`,
+      image: {
+        url: personnage.image
+      },
+      fields: []
+    }
+    for (const item of personnage.items) {
+      characterEmbed.fields.push({
+        name: `${interaction.client.getRarityEmoji(item.rarity)} ${interaction.client.getRarityItem(item.rarity)}`,
+        value: `#${item.id} ${item.name.upperCaseFirstLetter()}`
+      });
+    }
+    await interaction.editReply({embeds: [characterEmbed]});
   }
 }
