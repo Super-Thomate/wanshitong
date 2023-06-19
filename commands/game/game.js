909ecbf2aced1a2048f57cece09405e8d17d544f
Thomas Plaindoux <t.plaindoux@cartoon-fantasy.net>
2023-06-19 16:51:35 +0200

    redo the leaderboard after load/unload

--- commands/game/game.js
+++ commands/game/game.js
@@ -87,10 +87,11 @@ module.exports = {
         for (const row of leaderboard) {
           const trueNumberOfItems = await getInventoryCount(interaction.guildId, row.ownerId) ;
           if(trueNumberOfItems !== row.items) {
-            // console.log ('Difference !') ;
-            await Leaderboard.update({items: trueNumberOfItems}, {
-              where: {[Op.and]: [{ownerId: row.ownerId}, {guildId: interaction.guildId}]}
-            });
+            console.log ('Difference !') ;
+            console.log (`Get ${row.items}, awaiting ${trueNumberOfItems}`) ;
+            // await Leaderboard.update({items: trueNumberOfItems}, {
+            //   where: {[Op.and]: [{ownerId: row.ownerId}, {guildId: interaction.guildId}]}
+            // });
           }
         }
       } catch (err) {
