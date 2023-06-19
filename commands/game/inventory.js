0db6dffe41cb776e685132b82760ca19c5182fc4
Thomas Plaindoux <t.plaindoux@cartoon-fantasy.net>
2023-06-19 16:27:46 +0200

    debug inventory

--- commands/game/inventory.js
+++ commands/game/inventory.js
@@ -132,9 +132,9 @@ module.exports = {
         }
       }]
     });
-    // console.log(inventory);
+    console.log(inventory);
     // return await interaction.editReply('DEBUG');
-    console.log(inventory.length);
+    // console.log(inventory.length);
     // console.log(personnages);
     const numPage = Math.ceil (inventory / maxPerPage) || 1 ;
     const embedInventory = {
